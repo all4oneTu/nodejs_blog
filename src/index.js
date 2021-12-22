db3f71750a661f5dd870cb61f4c7b616e1922d73
all4oneTu <81431586+all4oneTu@users.noreply.github.com>
2021-12-22 22:23:28 +0700

    update express handlebar

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+const handlebars = require('express-handlebars')
+
+//express
+const app = express()
+const port = 3000
+
+//morgan
+app.use(morgan('combined'))
+
+//handlebar
+app.engine('handlebars', handlebars())
+app.set('view engine','handlebars')
+app.set('views', path.join(__dirname, 'resource/views'))
+
+//main code
+app.get('/', (req, res) => {
+  res.render('home')
+})
+app.get('/news', (req, res) => {
+  res.render('new')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
